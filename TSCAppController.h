73fcbf9decd8b618f92936f0021a4caaf3d67022
Jan Weiß <jan@geheimwerk.de>
2015-12-28 20:59:09 +0100

    Adding TSCAppController.

---
+++ TSCAppController.h
@@ -0,0 +1,13 @@
+//
+//  TSCAppController.h
+//  Transcriptions
+//
+//  Created by Jan on 28.12.15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TSCAppController : NSObject
+
+@end
---
+++ TSCAppController.m
@@ -0,0 +1,23 @@
+//
+//  TSCAppController.m
+//  Transcriptions
+//
+//  Created by Jan on 28.12.15.
+//
+//
+
+#import "TSCAppController.h"
+
+@implementation TSCAppController
+
+- (instancetype)init
+{
+	self = [super init];
+	
+	if (self) {
+	}
+	
+	return self;
+}
+
+@end
